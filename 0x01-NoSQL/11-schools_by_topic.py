8cb80617ecdde5751f8c9f11b9388dea9b25c15b
mwunnam <mwunnam@gmail.com>
2024-07-18 15:08:10 +0000

    done with task 11

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
+from typing import List, Dict, Any
 '''
 Retuns a list of school having the specific topic
 '''
 
 
-def schools_by_topic(mongo_collection, topic):
+def schools_by_topic(mongo_collection: Collection, topic: str) -> List[Dict[str, Any]]:
+
     '''
     Function that retuns a list  of school depending on the
     topic requested
