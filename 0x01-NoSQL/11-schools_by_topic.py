b1a65ef659ac157f82a21d734ab4bdcb33aa7d6e
mwunnam <mwunnam@gmail.com>
2024-07-18 13:46:30 +0000

    Done with task 11

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,13 @@
+#!/bin/env python3
+"""
+Retuns a list of school having the specific topic
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Function that retuns a list  of school depending on the
+    topic requested
+    """
+    schools = mongo_colloection.find({"topics": topic})
+    return list(schools)
