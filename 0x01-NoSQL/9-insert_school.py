9a2aa25f620d78debe4ded01cbb0f8258713a306
mwunnam <mwunnam@gmail.com>
2024-07-18 12:19:53 +0000

    correction on task 9

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,15 +1,7 @@
 #!/usr/bin/env python3
-from pymongo import MongoClient
-"""
-Inserting a new document in a collection based on kwargs:
-"""
+""" MongoDB Operations with Python using pymongo """
 
 
 def insert_school(mongo_collection, **kwargs):
-    """
-    Function inserts a new document into a given collection
-    the data is stored in a kwargs
-    Returns the id of the object of the inserted document
-    """
-    new_document = mongo_collection.insert_one(kwargs)
-    return new_document
+    """ Inserts a new document in a collection based on kwargs """
+     return mongo_collection.insert(kwargs)
