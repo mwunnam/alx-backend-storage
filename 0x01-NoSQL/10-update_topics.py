09fc810d4f8b148b59f983caf2c46b9ca89d3d76
mwunnam <mwunnam@gmail.com>
2024-07-18 13:01:04 +0000

    correction task 10

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
+from pymongo import MongoClient
 """
 Script that changes all topic base on the name selected
 """
 
 
 def update_topics(mongo_collection, name, topics):
+    """
+    Function changes the topics of a collection's document based
+    on the name given
+    """
     result = mongo_collection.update_Many(
         {"name": name},
         {"$set": {"topics": topics}}
     )
-    return result
