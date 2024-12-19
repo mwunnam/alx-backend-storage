eb2471b1c7b0aed316244623f5bc7f73f81ccce3
mwunnam <mwunnam@gmail.com>
2024-12-19 15:36:20 +0100

    correction

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,6 @@
 -- Ranking Origin bands, ordered by the number of fans
 
-SELECT orgin, SUM(fans) AS nb_fans
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC
